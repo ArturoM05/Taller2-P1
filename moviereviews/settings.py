45579b1d98811303d0717b5e525ac2dd222b998b
Arturo Fernando Murgueytio Arias <afmurgueya@eafit.edu.co>
2025-03-01 12:38:10 -0500

    statistics

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "moviereviews", "static"),
+]
 
 # Application definition
 
